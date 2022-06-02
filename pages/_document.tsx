72709e09597a4d01ac2340c67a7b5d88d21cd66d
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2022-06-02 08:33:48 -0300

    Update fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,7 +17,7 @@ class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
         <body>
